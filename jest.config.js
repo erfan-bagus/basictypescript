3b23f037de4000954b3e0041dfa7c3ea282853a5
erfan-bagus <erfanbagus@hotmail.com>
2022-03-02 21:18:59 +0700

    default typescript

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sync object
+var config = {
+    verbose: true,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+};
+exports.default = config;
+//# sourceMappingURL=jest.config.js.map
